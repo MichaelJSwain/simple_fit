77795e52efd240f3c040acc72509010dd187787c
MichaelSwain <michaeljswain@gmail.com>
2025-02-01 09:28:19 +0100

    add 5px row gap for WorkoutGoals component

--- src/components/WorkoutGoals.jsx
+++ src/components/WorkoutGoals.jsx
@@ -1,6 +1,6 @@
 const WorkoutGoals = ({goals}) => {
     return (
-        <div style={{display: "flex"}}>
+        <div style={{display: "flex", flexWrap: "wrap", rowGap: "5px"}}>
             {goals.map(goal => {
                 return <div key={goal} style={{marginRight: "5px", background: "grey", padding: "5px", fontSize: "12px", color: "black"}}>{goal}</div>
             })}
