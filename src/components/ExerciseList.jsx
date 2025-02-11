b178d56cd4e299d79828da67755ebeb57fd89f0e
MichaelSwain <michaeljswain@gmail.com>
2025-02-11 06:36:35 +0100

    filter exerciselist when user types in search input

--- src/components/ExerciseList.jsx
+++ src/components/ExerciseList.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react"
+
 export const ExerciseList = ({selectedExercises, exerciseList, onExerciseSelected}) => {
+    const [searchValue, setSearchValue] = useState("");
+    const [filteredExercises, setFilteredExercises] = useState(exerciseList);
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+    }
+
+    const handleSearchFilter = () => {
+        const filtered = exerciseList.filter(exercise => exercise.name.toLowerCase().includes(searchValue.toLowerCase()));
+        setFilteredExercises(filtered);
+    }
+
+    useEffect(handleSearchFilter, [searchValue]);
+
     return (
         <div>
-            <input placeholder="search" style={{borderRadius: "30px", padding: "15px 25px", width: "80%"}}></input>
-            {exerciseList.map(exercise => {
+            <input placeholder="search" style={{borderRadius: "30px", padding: "15px 25px", width: "80%"}} value={searchValue} onChange={handleChange}></input>
+            {filteredExercises.length ? filteredExercises.map(exercise => {
                 return <div key={exercise._id} onClick={() => onExerciseSelected(exercise)}>{exercise.name}</div>
-            })}
+            }) : <div>Sorry, there were no results found. Try another search term!</div>}
         </div>
     )
 }
